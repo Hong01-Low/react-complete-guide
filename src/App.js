8dfe8618c3525be6760e5d4b0c37f77bb5b42c18
Chun Hong <junhong.low123@gmail.com>
2021-05-21 22:32:57 +0800

    Do some adding and modify coding.

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
     };
 
     fetchTasks(
-      { url: 'https://react-http-6b4a6.firebaseio.com/tasks.json' },
+      { url: 'https://react-http-61334-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json' },
       transformTasks
     );
   }, [fetchTasks]);
