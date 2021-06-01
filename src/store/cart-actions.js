42daea862e236adc3294337c1cb61e353355539e
Chun Hong <junhong.low123@gmail.com>
2021-06-01 19:01:17 +0800

    Change coding

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -5,7 +5,7 @@ export const fetchCartData = () => {
   return async (dispatch) => {
     const fetchData = async () => {
       const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/cart.json'
+        'https://react-http-61334-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json'
       );
   
       if (!response.ok) {
