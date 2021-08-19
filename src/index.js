358a5fb8197b9c5bd2222d7611011b15743c03da
Chun Hong <junhong.low123@gmail.com>
2021-08-19 19:22:54 +0800

    Bye

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import './index.css';
-import App from './App';
-import store from './store/index';
+import "./styles.css";
+import App from "./App";
 
+const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <Provider store={store}>
+  <React.StrictMode>
     <App />
-  </Provider>,
-  document.getElementById('root')
+  </React.StrictMode>,
+  rootElement
 );
